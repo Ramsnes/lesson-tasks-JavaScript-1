fada5e69b4f87dc18071e4ac9897c5a80656e883
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-03-24 13:20:46 +0100

    rep M1L3

---
+++ M1L3creatingHtmlfromObjects/js/repProduct.js
@@ -0,0 +1,34 @@
+const product = {
+  name: "Fancy Product",
+  price: 99,
+  onSale: true,
+};
+
+let container = document.querySelector(".container");
+
+let name = product.name;
+let price = product.price;
+let onSale = product.onSale;
+
+// Price default value if missing
+if (!price) {
+  price = 666;
+}
+
+// Change color if onSale
+let color = onSale ? "red" : "green"; // true = red, else = green
+
+// Change innerText if onSale
+if (onSale) {
+  onSale = "On sale";
+} else {
+  onSale = "Regular price";
+}
+
+let decoration = onSale ? "underline" : "none"; // true = underline, else = none
+
+container.innerHTML = `<div class="product">
+<h2>${name}</h2>
+<p>${price}</p>
+<p style="text-decoration:${decoration}; color: ${color}">${onSale}</p>
+</div>`;
---
+++ M1L3creatingHtmlfromObjects/js/repProducts.js
@@ -0,0 +1,45 @@
+const products = [
+  {
+    name: "Fancy Product",
+    price: 40,
+    soldOut: true,
+  },
+  {
+    name: "Another Product",
+    soldOut: false,
+  },
+  {
+    name: "Cheap Product",
+    price: 10,
+    soldOut: true,
+  },
+];
+
+// Loop through array
+// Display each object using HTML
+
+let container = document.querySelector(".container");
+
+let html = "";
+
+for (let i = 0; i < products.length; i++) {
+  let name = products[i].name;
+  let price = products[i].price;
+  let soldOut = products[i].soldOut;
+
+  // If price is missing
+  if (!price) {
+    price = 100;
+  }
+
+  // Name color if sold out - Ternary
+  let color = soldOut ? "red" : "green"; // True = red, else = green
+
+  html += `<div class="products"> 
+            <h3 style="color: ${color}">${name}</h3>
+            <p>${price}</p>
+            <p>${soldOut}<p>
+        </div>`;
+}
+
+container.innerHTML = html;
